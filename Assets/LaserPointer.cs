69c3b53ae20cd456784debcfb9fc280b313a62a7
Jordan Medlock <jordan@innovo.net>
2018-06-18 10:45:53 -0600

    V1 Finished

---
+++ Assets/LaserPointer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class LaserPointer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        VRTK_Pointer pointer = GetComponent<VRTK_Pointer>();
+        
+        pointer.DestinationMarkerHover += DestinationMarkerHover;
+        pointer.DestinationMarkerSet += DestinationMarkerSet;
+    }
+
+    void DestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
+    {
+        Target target = e.target.GetComponent<Target>();
+        if (target)
+        {
+            target.HoverOn(e.destinationPosition, e.distance);
+        }
+    }
+    void DestinationMarkerSet(object sender, DestinationMarkerEventArgs e)
+    {
+        Target target = e.target.GetComponent<Target>();
+        if (target)
+        {
+            target.MarkerSetOn(e.destinationPosition, e.distance);
+        }
+    }
+    
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	public void HoverOn(Vector3 worldPosition, float distance)
+    {
+        Debug.Log("HoverOn() "+worldPosition+" "+distance);
+    }
+
+    public void MarkerSetOn(Vector3 worldPosition, float distance)
+    {
+        Debug.Log("MarkerSetOn() " + worldPosition + " " + distance);
+
+    }
+}
